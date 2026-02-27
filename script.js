cfb4997e98ef148e555d6b3beadd81a6a7d363e7
ShubhraTripathi-tech <shubhra.tripathi1611@gmail.com>
2026-02-27 06:55:14 +0000

    add image code in script.js

--- script.js
+++ script.js
@@ -2,71 +2,54 @@ const fruitForm = document.querySelector("#inputSection form");
 const fruitList = document.querySelector("#fruitSection ul");
 const fruitNutrition = document.querySelector("#nutritionSection p");
 
+let cal = 0;
+const fruitCal = {};
+const apiKey = "54810753-c970ae0e97a375af20d3dff02";
+
 fruitForm.addEventListener("submit", extractFruit);
 
 function extractFruit(e) {
   e.preventDefault();
-  fetchFruit(e.target.fruitInput.value);
+  fetchFruitData(e.target.fruitInput.value);
   e.target.fruitInput.value = "";
 }
 
-let cal = 0;
-
-function addFruit(fruit) {
-  const li = document.createElement("li");
-  li.textContent = fruit.name;
-  const imgurl =
-    "https://pixabay.com/api/key=54810753-c970ae0e97a375af20d3dff02&q=${fruit}";
-  const img = document.createElement("img");
-  img.src = imgurl;
-  li.appendChild(img);
-  li.addEventListener("click", removeFruit);
-  fruitList.appendChild(li);
-  cal += fruit.nutritions.calories;
-  fruitNutrition.textContent = cal;
-}
-
-async function fetchFruit(fruit) {
+async function fetchFruitData(fruit) {
   try {
-    const resp = await fetch(
+    const respData = await fetch(
       `https://fruit-api-5v0j.onrender.com/fruits/${fruit}`,
     );
-    if (resp.ok) {
-      const data = await resp.json();
-      addFruit(data);
-      addImage(data);
+    const respImg = await fetch(
+      `https://pixabay.com/api/?q=${fruit}+fruit&key=${apiKey}`,
+    );
+    if (respData.ok && respImg.ok) {
+      const data = await respData.json();
+      const imgData = await respImg.json();
+      addFruit(data, imgData);
     } else {
-      throw `Error: http status code = ${resp.status}`;
+      throw "something went wrong with one of the API requests";
     }
   } catch (err) {
     console.log(err);
   }
-}
-/*
-function addImage(fruit) {
-  const img = document.createElement("img");
-  img.src = fruit.image;
 }
 
-async function fetchImage(fruit) {
-  try {
-    const resp = await fetch(
-      `https://pixabay.com/api/key=54810753-c970ae0e97a375af20d3dff02&q=${fruit}`,
-    );
-    if (resp.ok) {
-      const data = await resp.json();
-      addImage(data);
-    } else {
-      throw `Error: http status code = ${resp.status}`;
-    }
-  } catch (err) {
-    console.log(err);
-  }
+function addFruit(fruit, fruitImg) {
+  const img = document.createElement("img");
+  img.classList.add("fruits");
+  img.alt = fruit.name;
+  img.src = fruitImg.hits[0].webformatURL;
+
+  img.addEventListener("click", removeFruit, { once: true });
+  fruitList.appendChild(img);
+  fruitCal[fruit.name] = fruit.nutritions.calories;
+  cal += fruit.nutritions.calories;
+  fruitNutrition.textContent = "Total Calories: " + cal;
 }
-  */
 
 function removeFruit(e) {
-  cal -= e.target.nutritions.calories;
-  fruitNutrition.textContent = cal;
+  const fruitName = e.target.alt;
+  cal -= fruitCal[fruitName];
+  fruitNutrition.textContent = "Total Calories: " + cal;
   e.target.remove();
 }
